c50e724829d48bf115b7232110163eded85b2e4b
MuhidinM <misbahmuhidin3@gmail.com>
2024-02-03 21:16:34 +0300

    heading

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl">
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">NoteWorth</span>
+      </h1>
+    </div>
+  );
+};
